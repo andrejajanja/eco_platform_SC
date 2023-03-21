09737a27efff0321039c5c5699946ebc46b82473
Andreja <andrejajanja@gmail.com>
2023-03-21 14:54:04 +0100

    started doing infoWindows

--- server_files/static/js/map_g_api.js
+++ server_files/static/js/map_g_api.js
@@ -3,14 +3,28 @@ function mapInitialization() {
     const mapica = new google.maps.Map(document.getElementById("mapa"), {
         zoom: 5,
         center: centar,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        mapTypeControl: false,
+        streetViewControl: false
       });
     let markeri = [];
     events.forEach(function (elem){
-        markeri.push(new google.maps.Marker({
+        let pomMarker;
+        let pomInfo;
+        pomInfo = new google.maps.InfoWindow({
+            content: `Event type: ${elem[1]}` //finnish content of this also close all the opened infoWindows if new marker is clicked
+        })
+        pomMarker = new google.maps.Marker({
             position: elem[0],
             icon: `/static/images/${elem[1]}32.png`
-        }));
+        })
+        pomMarker.addListener("click", ()=>{
+            pomInfo.open({
+                anchor: pomMarker, mapica
+            });
+        });
+
+        markeri.push(pomMarker);
     });
     markeri.forEach(function (elem){       
         elem.setMap(mapica);
